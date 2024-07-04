5ed471aea5e11f36394693880943c381af3cfeda
Specify your username <Your email address>
2024-07-04 20:13:06 +0200

    fixed errors

--- src/components/WorkComp.vue
+++ src/components/WorkComp.vue
@@ -5,9 +5,8 @@
         <div v-for="(experience, index) in workExperienceData" :key="index" class="work-section__content">
           {{ experience.description }}
         </div>
-        <a :href="'https://drive.google.com/uc?export=download&id=1U5yXvrhWjpM1mXatog0SShmAxrkitnKI'" download="CV.pdf" class="download-cv-btn">
-          <button>Download CV</button>
-        </a>
+        <br>
+        <button @click="downloadCV">Download CV</button>
       </div>
     </div>
   </template>
@@ -21,12 +20,29 @@
     },
     mounted() {
       fetch("https://jords-springy.github.io/first_api/data/index.json?_=" + new Date().getTime())
-      .then(response => response.json())
-      .then(data => {
+        .then(response => response.json())
+        .then(data => {
           this.workExperienceData = data.workExperience;
         })
-      .catch(error => console.error(error));
+        .catch(error => console.error(error));
     },
+    methods: {
+      downloadCV() {
+        const url = 'https://jords-springy.github.io/hostedimages/images/CV-Jordan-Springveldt.pdf';
+        const filename = 'CV-Jordan-Springveldt.pdf';
+        fetch(url)
+          .then(response => response.blob())
+          .then(blob => {
+            const link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = filename;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+          })
+          .catch(console.error);
+      }
+    }
   };
   </script>
   
@@ -47,12 +63,7 @@
       font-size: 1.2rem;
     }
     
-    .download-cv-btn {
-      display: block;
-      margin-top: 20px;
-    }
-    
-    .download-cv-btn button {
+    button {
       background-color: #a6a6a6;
       color: #fff;
       padding: 10px 20px;
@@ -61,59 +72,8 @@
       cursor: pointer;
     }
     
-    .download-cv-btn button:hover {
+    button:hover {
       background-color: #6d5b67;
     }
-    
-    /* For screens larger than 1200px */
-    @media (min-width: 1200px) {
-      .work-section {
-        padding: 80px 0;
-      }
-      .work-section__title {
-        font-size: 3rem;
-      }
-      .work-section__content {
-        font-size: 1.3rem;
-      }
-    }
-    
-    /* For screens between 768px and 1200px */
-    @media (min-width: 768px) and (max-width: 1200px) {
-      .work-section {
-        padding: 60px 0;
-      }
-      .work-section__title {
-        font-size: 2.5rem;
-      }
-      .work-section__content {
-        font-size: 1.2rem;
-      }
-    }
-    
-    /* For screens smaller than 768px */
-    @media (max-width: 768px) {
-      .work-section {
-        padding: 40px 0;
-      }
-      .work-section__title {
-        font-size: 2rem;
-      }
-      .work-section__content {
-        font-size: 1.1rem;
-      }
-    }
-    
-    /* For screens smaller than 300px */
-    @media (max-width: 300px) {
-      .work-section {
-        padding: 20px 0;
-      }
-      .work-section__title {
-        font-size: 1.5rem;
-      }
-      .work-section__content {
-        font-size: 1rem;
-      }
-    }
   </style>
+  
