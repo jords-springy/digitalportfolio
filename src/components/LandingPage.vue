2b6945a229d18b873e83d85036592f804992718b
jords-springy <jordanspringveldt@gmail.com>
2024-11-01 08:45:37 +0100

    😊

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -21,7 +21,7 @@
                 </div>
               </h2>
               <h4 class="card-text" style="color: #6d5b67">
-                A Life Choices Student + Aspiring Web Developer
+                A LC Studio Intern + Aspiring Web Developer
               </h4>
               <button
                 class="btn btn-light mt-3"
